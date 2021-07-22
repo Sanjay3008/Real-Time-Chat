0dcce969cc6fef2b5aa72e4ddc7e9d9742116fbd
SANJAY <ssanjaynkl1999@gmail.com>
2021-07-22 20:04:02 +0530

    make it better

--- Public/js/chat.js
+++ Public/js/chat.js
@@ -132,3 +132,10 @@ function outputUsers(users) {
 }
 
 console.log(document.getElementById('group-name-t').textContent);
+
+
+document.documentElement.style.setProperty('overflow','auto');
+
+const metaViewport = document.querySelector('meta[name=viewport]');
+
+metaViewport.setAttribute('content','height='+ initialHeight+ 'px,width=device-width,initial-scale=1.0');
